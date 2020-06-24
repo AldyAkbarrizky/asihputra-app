4cd1b9c5c5183381320cf5f159276b63e513fdab
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2020-06-24 15:12:10 +0700

    Updates

---
+++ precache-manifest.f5e0ea68e30f5fca97ff74505cf83f0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343e43f645d202a1858305430696abf3",
+    "url": "/asihputra-app/index.html"
+  },
+  {
+    "revision": "04f875777723399de96b",
+    "url": "/asihputra-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "914ef2ed03a7a95874f9",
+    "url": "/asihputra-app/static/js/2.67f26d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asihputra-app/static/js/2.67f26d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f875777723399de96b",
+    "url": "/asihputra-app/static/js/main.966b0487.chunk.js"
+  },
+  {
+    "revision": "d80639ba1ecb25a5fc0a",
+    "url": "/asihputra-app/static/js/runtime-main.2f44b0e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/asihputra-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
